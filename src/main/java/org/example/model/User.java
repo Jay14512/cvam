cb2e921a5aaad6dedab9fc57683556dc5638d174
Joshua Jason <jjm40971@gmail.com>
2026-07-07 18:12:47 +0200

    first commit

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class  User {
+    private String name;
+    private String email;
+
+    public User (String name, String email){
+        this.name=name;
+        this.email=email;
+    }
+
+}
